b75d19fe00492373d61411219d248bb9532af96b
monukumar98 <monu012as@gmail.com>
2023-05-21 12:26:49 +0530

    Operators

--- Lec1/Increment_Decrement.java
+++ Lec1/Increment_Decrement.java
@@ -9,8 +9,12 @@ public class Increment_Decrement {
 		int x = 6;
 //        x++;// x = x+1;
 //        ++x;// // x = x+1;
-		System.out.println(x++);
-		System.out.println(x);
+//		System.out.println(x++);
+//		System.out.println(x);
+		int y = x++ + 4 + x-- - x-- + 7 + ++x + --x;
+//		System.out.println(++x);
+//		System.out.println(x);
+		System.out.println(y);
 
 	}
 
