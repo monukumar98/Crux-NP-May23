70718241b7bb7f160abfa220d3c4d8e94e5deff9
monukumar98 <monu012as@gmail.com>
2023-07-02 09:55:02 +0530

    Binary Search

---
+++ Lec11/Binary_Search.java
@@ -0,0 +1,27 @@
+package Lec11;
+
+public class Binary_Search {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 2, 3, 5, 6, 7, 11, 13, 15, 18, 19, 20 };
+		int item = 13;
+		System.out.println(Search(arr, item));
+	}
+	public static int Search(int[] arr, int item) {
+		int lo = 0;
+		int hi = arr.length - 1;
+		while (lo <= hi) {
+			int mid = (lo + hi) / 2;
+			if (arr[mid] == item) {
+				return mid;
+			} else if (arr[mid] > item) {
+				hi = mid - 1;
+			} else {
+				lo = mid + 1;
+			}
+		}
+		return -1;
+
+	}
+}
